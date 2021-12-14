904dd051eab59b21c49d9f69366c06d269611497
Zubair Shaikh <zsescape@gmail.com>
2021-12-14 06:38:56 -0500

    Add login and signup

--- app.py
+++ app.py
@@ -1,6 +1,6 @@
 from datetime import datetime
 
-from flask import Flask, jsonify, render_template
+from flask import Flask, jsonify, render_template, request
 
 app = Flask(__name__)
 
@@ -17,6 +17,21 @@ def status():
 def index():
     return render_template("index.html")
 
+
+@app.route("/login")
+def login():
+    if request.method == "GET":
+        return render_template("login.html")
+    elif request.method == "POST":
+        pass
+
+
+@app.route("/signup")
+def signup():
+    if request.method == "GET":
+        return render_template("signup.html")
+    elif request.method == "POST":
+        pass
 
 
 if __name__ == "__main__":
