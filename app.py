ed74c87f9f4ece558a4ae668fefa14b6bc78ef16
Zubair Shaikh <zsescape@gmail.com>
2021-12-14 07:29:27 -0500

    Add game, login, signup forms

--- app.py
+++ app.py
@@ -1,6 +1,8 @@
 from datetime import datetime
+from pprint import pprint
 
 from flask import Flask, jsonify, render_template, request
+
 
 app = Flask(__name__)
 
@@ -18,20 +20,40 @@ def index():
     return render_template("index.html")
 
 
-@app.route("/login")
+@app.route("/login", methods=["GET", "POST"])
 def login():
     if request.method == "GET":
         return render_template("login.html")
     elif request.method == "POST":
-        pass
+        pprint(request.form)
+        return jsonify(request.form)
 
 
-@app.route("/signup")
+@app.route("/signup", methods=["GET", "POST"])
 def signup():
     if request.method == "GET":
         return render_template("signup.html")
     elif request.method == "POST":
-        pass
+        pprint(request.form)
+        return jsonify(request.form)
+
+
+@app.route("/game", methods=["GET", "POST"])
+def game():
+    question = {
+        "id": 1,
+        "question": "What is the capital of India ?",
+        "A": "Delhi",
+        "B": "Mumbai",
+        "C": "Koklata",
+        "D": "Chennai"
+    }
+
+    if request.method == "GET":
+        return render_template("game.html", question=question)
+    elif request.method == "POST":
+        pprint(request.form)
+        return jsonify(request.form)
 
 
 if __name__ == "__main__":
