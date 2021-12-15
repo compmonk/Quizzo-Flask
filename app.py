632006aa65563d78f8d0b4bcb9f6d1f9d6135cae
Zubair Shaikh <zsescape@gmail.com>
2021-12-14 21:29:07 -0500

    Add questions and scores

--- app.py
+++ app.py
@@ -3,9 +3,8 @@ from os import error
 from pprint import pprint
 from MySQLdb.cursors import Cursor
 
-from flask import Flask, jsonify, render_template, request, session
+from flask import Flask, jsonify, render_template, request, session, redirect
 from flask_mysqldb import MySQL
-from werkzeug.utils import redirect
 
 
 app = Flask(__name__)
@@ -17,21 +16,12 @@ app.config['MYSQL_DB'] = 'quizzo'
 
 mysql = MySQL(app)
 
-
-question = {
-    "id": 1,
-    "question": "What is the capital of India ?",
-    "A": "Delhi",
-    "B": "Mumbai",
-    "C": "Koklata",
-    "D": "Chennai"
-}
-
 @app.route("/status")
 def status():
     return jsonify({
         "success": True,
-        "time": datetime.now().isoformat()
+        "time": datetime.now().isoformat(),
+        "user": session.get("user")
     })
 
 
@@ -51,7 +41,15 @@ def login():
             cursor.execute("SELECT * FROM users WHERE email='{0}'".format(form.get("email", "")))
             user = cursor.fetchone()
             if user:
-                session["user"] = {"id": user[0], "first_name": user[1], "last_name": user[2], "email": user[3]}
+                pprint(user)
+                session["user"] = {
+                    "id": user[0], 
+                    "first_name": user[1], 
+                    "last_name": user[2], 
+                    "email": user[3],
+                    "questions_played": user[4],
+                    "score": user[5]
+                    }
             cursor.close()
 
             return redirect("/game")
@@ -75,8 +73,16 @@ def signup():
 
             cursor.execute("SELECT * FROM users WHERE email='{0}'".format(form.get("email", "")))
             user = cursor.fetchone()
+            pprint(user)
             if user:
-                session["user"] = {"id": user[0], "first_name": user[1], "last_name": user[2], "email": user[3]}
+                session["user"] = {
+                    "id": user[0], 
+                    "first_name": user[1], 
+                    "last_name": user[2], 
+                    "email": user[3],
+                    "questions_played": user[4],
+                    "score": user[5]
+                    }
             cursor.close()
 
             return redirect("/game")
@@ -88,12 +94,43 @@ def signup():
 def game():
     if request.method == "GET":
         if session.get("user"):
-            return render_template("game.html", question=question)
+            cursor = mysql.connection.cursor()
+            cursor.execute("SELECT * FROM questions WHERE id={0}".format(session["user"]["questions_played"] + 1))
+            question = cursor.fetchone()
+            if question:
+                session["question"] = {
+                    "id": question[0], 
+                    "question": question[1], 
+                    "A": question[2], 
+                    "B": question[3], 
+                    "C": question[4], 
+                    "D": question[5], 
+                    "answer": question[6]
+                    }
+            cursor.close()
+            return render_template("game.html")
         else:
             return redirect("/login")
     elif request.method == "POST":
-        pprint(request.form)
-        return jsonify(request.form)
+        form = request.form
+        try:
+            # session["user"]["questions_played"] = session["user"]["questions_played"] + 1
+            session["user"]["questions_played"] += 1
+            session["user"]["score"] += int(form["answer"] == session["question"]["answer"])
+            session.modified = True
+
+            cursor = mysql.connection.cursor()
+            query = "UPDATE users SET questions_played = {0}, score = {1} WHERE id={2}".format(
+                session["user"]["questions_played"],
+                session["user"]["score"],
+                session["user"]["id"]
+                )
+            cursor.execute(query)
+            mysql.connection.commit()
+            cursor.close()
+            return redirect("/game")
+        except error:
+            return jsonify(error)
 
 
 @app.route("/logout")
